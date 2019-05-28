76126b958142a1cef8e8ee4b287195ef8ed71d43
alex-mlg <42219295+alex-mlg@users.noreply.github.com>
2019-05-28 16:56:37 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common \
-    htop
+#sudo apt-get remove docker docker-engine docker.io containerd runc
+#sudo apt-get update
+#sudo apt-get install \
+#    apt-transport-https \
+#    ca-certificates \
+#    curl \
+#    gnupg-agent \
+#    software-properties-common \
+#    htop
     
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -34,3 +34,6 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo usermod -aG docker $USER
+
+pip install --upgrade pip
+pip install selenium==3.141.0
